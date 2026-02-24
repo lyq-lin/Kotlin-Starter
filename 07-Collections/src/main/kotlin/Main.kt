94e32eb3621ced9a024b2eab3f466cbb4c771515
linyuqiao <linyuqiao@appeon.net.com>
2026-02-24 15:17:10 +0800

    chore: 添加map案例

--- 07-Collections/src/main/kotlin/Main.kt
+++ 07-Collections/src/main/kotlin/Main.kt
@@ -15,6 +15,12 @@ fun main() {
     // map + filter
     val onlyWarnings = logs.map { it.uppercase() }.filter { it.startsWith("W") }
 
+    val m = mapOf<Int, String>(1 to "hello", 2 to "world")
+
+    for ((index,word) in m) {
+        println("$index: $word")
+    }
+
     // fold: 从左到右聚合
     val totalChars = logs.fold(0) { acc, level -> acc + level.length }
 
